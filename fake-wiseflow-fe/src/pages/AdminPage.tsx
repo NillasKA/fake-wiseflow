1b7a57df098e9447e0a42cde45b769307746a14c
Hadi3232 <hadibusiness1@hotmail.com>
2025-11-25 10:16:29 +0100

    opret eksamen done

--- fake-wiseflow-fe/src/pages/AdminPage.tsx
+++ fake-wiseflow-fe/src/pages/AdminPage.tsx
@@ -1,12 +1,14 @@
 import "../stylesheets/pages/AdminPage.css"
 import InstitutionModule from "../components/administration/InstitutionModule.tsx";
 import UserModule from "../components/administration/UserModule";
+import ExamModule from "../components/administration/ExamModule.tsx";
 
 export default function AdminPage() {
     return (
         <div className="admin-container">
             <InstitutionModule />
             <UserModule />
+            <ExamModule />
         </div>
     );
 }
