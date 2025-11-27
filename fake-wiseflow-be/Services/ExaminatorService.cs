1b3d65230ddb793227189231bc3f4644ee0a239e
Spaxyq <jona44jensen@gmail.com>
2025-11-27 11:03:46 +0100

    Fixed password generation logic to ensure password requirements

--- fake-wiseflow-be/Services/ExaminatorService.cs
+++ fake-wiseflow-be/Services/ExaminatorService.cs
@@ -147,20 +147,48 @@ public class ExaminatorService : IExaminatorService
 
     private static string GenerateSecurePassword(int length = 12)
     {
-        const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
+        if (length < 8)
+            throw new ArgumentException("Password length must be at least 8 characters", nameof(length));
+
+        const string lowercase = "abcdefghijklmnopqrstuvwxyz";
+        const string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string digits = "0123456789";
+        const string special = "!@#$%^&*";
+        const string allChars = lowercase + uppercase + digits + special;
+
+        using var rng = RandomNumberGenerator.Create();
         var password = new StringBuilder();
 
-        using (var rng = RandomNumberGenerator.Create())
-        {
-            var buffer = new byte[length];
-            rng.GetBytes(buffer);
+        password.Append(GetRandomChar(lowercase, rng));
+        password.Append(GetRandomChar(uppercase, rng));
+        password.Append(GetRandomChar(digits, rng));
+        password.Append(GetRandomChar(special, rng));
 
-            foreach (var b in buffer)
-            {
-                password.Append(validChars[b % validChars.Length]);
-            }
+        for (int i = 4; i < length; i++)
+        {
+            password.Append(GetRandomChar(allChars, rng));
         }
 
-        return password.ToString();
+        return Shuffle(password.ToString(), rng);
+    }
+
+    private static char GetRandomChar(string chars, RandomNumberGenerator rng)
+    {
+        var buffer = new byte[1];
+        rng.GetBytes(buffer);
+        return chars[buffer[0] % chars.Length];
+    }
+
+    private static string Shuffle(string str, RandomNumberGenerator rng)
+    {
+        var array = str.ToCharArray();
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            var buffer = new byte[1];
+            rng.GetBytes(buffer);
+            int j = buffer[0] % (i + 1);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+        return new string(array);
     }
 }
